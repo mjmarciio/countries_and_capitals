db1b0282328f7ce3ab18ab5b352d1b9b9319e1b4
Márcio Junior <marciomsgj@gmail.com>
2025-06-22 12:13:56 -0300

    adicionando assets e array de dados

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     echo 'Hello World';
 });
+
+Route::get('/show_data', [MainController::class, 'showData']);
