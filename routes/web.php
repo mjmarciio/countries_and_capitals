51df63390edad85602e1edfc29f6557670a3b1e0
Márcio Junior <marciomsgj@gmail.com>
2025-06-22 12:37:17 -0300

    adicionando os components inciais

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    echo 'Hello World';
-});
+Route::view('/', 'home');
 
 Route::get('/show_data', [MainController::class, 'showData']);
